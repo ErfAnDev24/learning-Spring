ac0e2b047f441e3aa4b8d979524b6d05f09c3749
ErfAn Pezeshkpour <www.erfan01724@gmail.com>
2022-02-05 16:34:30 +0330

    my first program with new Spring tutorial

---
+++ src/com/ErfAn/MAINAPP.java
@@ -0,0 +1,15 @@
+package com.ErfAn;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MAINAPP {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+	}
+}
