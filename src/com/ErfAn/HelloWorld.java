d7cefe6be0b288e5a3e1edc600fb04cc1f311d34
ErfAn Pezeshkpour <www.erfan01724@gmail.com>
2022-02-07 14:59:24 +0330

    change

--- src/com/ErfAn/HelloWorld.java
+++ src/com/ErfAn/HelloWorld.java
@@ -1,13 +1,5 @@
 package com.ErfAn;
 
-import javax.annotation.PostConstruct;
-
 public class HelloWorld {
-	
-	
-	@PostConstruct
-	public void MyMethod() {
-		System.out.println("Second");
-	}
-	
+
 }
---
+++ src/com/ErfAn/MySecond.java
@@ -0,0 +1,5 @@
+package com.ErfAn;
+
+public class MySecond {
+
+}
